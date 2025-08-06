4d5cd8e6db4079a0634b437e7d2ad5d5e67acd32
uiuxganisigit <ganysigit1@gmail.com>
2025-08-06 11:04:39 +0700

    Remove chart component from dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,4 @@
 import { AppSidebar } from "@/components/app-sidebar"
-import { ChartAreaInteractive } from "@/components/chart-area-interactive"
 import { IssuesDataTable } from "@/components/dashboard/issues-data-table"
 import { SectionCards } from "@/components/section-cards"
 import { SiteHeader } from "@/components/site-header"
@@ -43,9 +42,6 @@ export default async function Page() {
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
-              <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
-              </div>
               <div className="px-4 lg:px-6">
                 <IssuesDataTable data={issues} />
               </div>
